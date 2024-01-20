1134acec31ca59604c2545e8c95b4e86ea502e47
Creative-coder-zzzz <sandeshadhikari2003@gmail.com>
2024-01-20 13:22:58 +0530

    Final Js update

--- main.js
+++ main.js
@@ -2,20 +2,32 @@
 let butn = document.querySelector("#menu-icon");
 let swt = "yes";
 console.log(butn);
+butn.onclick = () =>{
+  butn.classList.toggle('bx-x');
+}
 butn.onclick =() =>{
   if(swt==="yes"){
     document.querySelector(".dropdown").style.display ="flex";
+    butn.classList.remove('bx-menu');
+    butn.classList.add('bx-x');
     swt = "no";
   }
   else{
     document.querySelector(".dropdown").style.display ="none";
+    butn.classList.remove('bx-x');
+    butn.classList.add('bx-menu');
     swt = "yes";
   }
 };
 
+//darkmode
+let darkmode = document.querySelector('#darkMode-icon');
+darkmode.onclick = () =>{
+  darkmode.classList.toggle('bx-sun');
+  document.body.classList.toggle('darkmode');
+} 
 
 //menu icon navbar
-
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () => {
@@ -50,3 +62,14 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+//scroll reveal
+ ScrollReveal({
+    reset: true,
+   distance: '80px',
+   duration: 2000,
+   delay: 200
+   });
+   ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+   ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form ',{ origin: 'bottom' });
+   ScrollReveal().reveal('.home-content h1, .about-img ', { origin: 'left' });
+   ScrollReveal().reveal('.home-content h3, .home-content p, .about ', { origin: 'right' });
